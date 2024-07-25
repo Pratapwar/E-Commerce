8756db5ff2d9ffdb8be6fa37a4b3fe7893248fef
Pratapwar <akhileshpratapwar123@gmail.com>
2024-07-25 14:00:38 +0530

    Changes in server

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path from "path";
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
@@ -15,21 +16,25 @@ dotenv.config();
 connectDB();
 
 //rest object
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
